18cb404cab9753b60747da0fd5f08117a9adc78a
SagarTalagatti <sagartalagatti@gmail.com>
2023-03-07 11:14:39 +0530

    Complete source code of Spring configuration using Annotations and Java

---
+++ src/com/luv2code/springdemo/SwimCoach.java
@@ -0,0 +1,42 @@
+package com.luv2code.springdemo;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class SwimCoach implements Coach {
+
+	private FortuneService fortuneService;
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
+
+	public SwimCoach(FortuneService theFortuneService) {
+		System.out.println(">> Inside ctor of SwimCoach");
+		fortuneService = theFortuneService;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+
+	@Override
+	public String getDailyWorkout() {
+		return "Swim 1000 meters as a warm up.";
+	}
+
+	@Override
+	public String getDailyFortune() {
+		return fortuneService.getFortune();
+	}
+
+}
+
+
+
+
